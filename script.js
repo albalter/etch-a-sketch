488900e64f35883c1c223a4eeddc5d8d2d6d73b9
Alex Balter <albalter@gmail.com>
2025-08-11 16:54:30 +0300

    Implement all requested features in etch a sketch with the possible exception of a board appearing straight away, slightly improving the overall UX Othr issue is the traces left by hvoering the mouse over the sheet, it might be better to only \ etch things in on a mousedown event.

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 
-const container = document.querySelector(".container")
+document.getElementById("resetBoard").onclick = function () {
+        resetBoard() 
+        let cellsWide = prompt("How many cells would you like to have on the board? (Please enter a number not greater than 100)")
+        cellsWide = cellsWide > 100 ? 100 : cellsWide
+        createBoard(cellsWide)
+     };
 
-for (let i=0;i<16;i++){
-    for (let j=0;j<16;j++){
+function resetBoard() { 
+    let container = document.querySelector(".container")
+    container.innerHTML = ""
+}
+
+function createBoard(cellsWide){
+
+let container = document.querySelector(".container")
+for (let i=0;i<cellsWide;i++){
+    for (let j=0;j<cellsWide;j++){
         let div = document.createElement("div")
         div.addEventListener("mouseover", (event) => {
-            div.style.background = "green";
+            div.style.opacity = String(Number(div.style.opacity) + 0.1);
         })
-        div.style.width = String(400/16)+'px'
-        div.style.height =String(400/16)+'px'
+        div.style.width = String(600/cellsWide)+'px'
+        div.style.height =String(600/cellsWide)+'px'
+        div.style.opacity = 0.2;
         div.style.background = "aquamarine"
         div.style.margin = "1px"
         container.appendChild(div)
     }
+}
+
 }
 
